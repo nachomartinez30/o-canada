491957e611b18414643edbc3277df5b2db743b74
Jorge <akiubarx@hotmail.com>
2020-05-20 16:50:59 -0500

    hotfix

--- src/components/S4.jsx
+++ src/components/S4.jsx
@@ -13,24 +13,24 @@ const S4 = (props) => {
     return (
         <div className='row'>
             {/* SCI/SMI 100 */}
-            <div className='col-12'>
+            <div className='col-6'>
                 <label className="control-label pt-2">SCI/SMI 100</label>
                 <input
                     className="form-control myInput"
                     name='sci-smi_100'
-                    type=''
+                    type='file'
                     onChange={setInfo}
                     placeholder='Ingrese SCI/SMI 100...'
                 />
             </div>
 
             {/* SCI/SMI 200 */}
-            <div className='col-12'>
+            <div className='col-6'>
                 <label className="control-label pt-2">SCI/SMI 200</label>
                 <input
                     className="form-control myInput"
                     name='sci-smi_200'
-                    type=''
+                    type='file'
                     onChange={setInfo}
                     placeholder='Ingrese SCI/SMI 200...'
                 />
