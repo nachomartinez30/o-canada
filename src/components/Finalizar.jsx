9be028535568cd6f49073ef23d2531ca6f5738ac
Ignacio <nachomartinez3010@gmail.com>
2020-05-20 15:20:24 -0500

    validaciones para ingresar datos S1

---
+++ src/components/Finalizar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Finalizar = () => {
+    return (
+        <div className='row'>
+            <h1>Finalizó captura</h1>
+        </div>
+    );
+}
+
+export default Finalizar;
