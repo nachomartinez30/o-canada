e9f9f0b9a48d2166cb931a957a826665d0c821e8
Ignacio <nachomartinez3010@gmail.com>
2020-05-21 12:55:14 -0500

    hotfix para botstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import './assets/styles.css'
 import Header from './singles/Header';
 import Captura from './rutas/Captura';
