04bd58901d37051c9165714a124691dda0945d48
Ignacio <nachomartinez3010@gmail.com>
2020-05-22 16:47:09 -0500

    validacione s4 y 5

--- src/components/S4.jsx
+++ src/components/S4.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SelectSiNo from '../singles/SelectSiNo'
 import SCI100 from '../components/ExSCI100';
 import S190 from '../components/ExS190';
+import AlertExito from '../singles/AlertExito';
 
 const S4 = (props) => {
     /* TODOD: rellenar state y API con esta seccion */
     const { state, setState, checkData } = props
+    const [preguntas_smi_100, setPreguntas_smi_100] = useState(false)
+    const [preguntas_s_190, setPreguntas_s_190] = useState(false)
+    const [smi_100Examen, setSmi_100Examen] = useState(false)
 
     const setInfo = (input) => {
         /* setea al state las variables */
@@ -14,19 +18,25 @@ const S4 = (props) => {
             [input.target.name]: input.target.value
         })
     }
+
+    const siguienteExamen = () => {
+
+        AlertExito('Examen')
+        setSmi_100Examen(true)
+    }
     return (
         <div className='row body_wrap'>
-            <div className='col-12'>
-                <h1>EXAMENES</h1>
-            </div>
+
             {/* SCI/SMI 100 */}
             <div className='col-6'>
                 <label className="control-label pt-2">SCI/SMI 100</label>
                 <input
                     className="form-control myInput"
-                    name='sci-smi_100'
+                    name='sci_smi_100'
                     type='file'
                     onChange={setInfo}
+                    onBlur={() => { setPreguntas_smi_100((state.sci_smi_100 && state.sci_smi_200) ? true : false) }}
+                    onMouseLeave={() => { setPreguntas_smi_100((state.sci_smi_100 && state.sci_smi_200) ? true : false) }}
                     placeholder='Ingrese SCI/SMI 100...'
                 />
             </div>
@@ -36,76 +46,97 @@ const S4 = (props) => {
                 <label className="control-label pt-2">SCI/SMI 200</label>
                 <input
                     className="form-control myInput"
-                    name='sci-smi_200'
+                    name='sci_smi_200'
                     type='file'
                     onChange={setInfo}
+                    onBlur={() => { setPreguntas_smi_100((state.sci_smi_100 && state.sci_smi_200) ? true : false) }}
+                    onMouseLeave={() => { setPreguntas_smi_100((state.sci_smi_100 && state.sci_smi_200) ? true : false) }}
                     placeholder='Ingrese SCI/SMI 200...'
                 />
             </div>
 
-            {/* ¿El evaluado ha participado en eventos planeados o no...? */}
-            <div className='col-12'>
-                <label className="control-label danger pt-2">¿El evaluado ha participado en eventos planeados o no planeados atendidos bajo el SCI?</label>
-                <SelectSiNo
-                    className="form-control myInput"
-                    name='eventos_plnaeados_sci'
-                    onChange={setInfo}
-                />
-            </div>
+            {preguntas_smi_100 && <React.Fragment>
+                {/* ¿El evaluado ha participado en eventos planeados o no...? */}
+                <div className='col-12'>
+                    <label className="control-label danger pt-2">¿El evaluado ha participado en eventos planeados o no planeados atendidos bajo el SCI?</label>
+                    <SelectSiNo
+                        className="form-control myInput"
+                        name='eventos_plnaeados_sci'
+                        onChange={setInfo}
+                    />
+                </div>
 
-            {/* ¿El evaluado ha participado en eventos planeados o no planeados...? */}
-            <div className='col-12'>
-                <label className="control-label danger pt-2">¿El evaluado ha participado en eventos planeados o no planeados atendidos bajo el SCI fuera de su país?</label>
-                <SelectSiNo
-                    className="form-control myInput"
-                    name='eventos_plnaeados_sci_fuera'
-                    onChange={setInfo}
-                />
-            </div>
+                {/* ¿El evaluado ha participado en eventos planeados o no planeados...? */}
+                <div className='col-12'>
+                    <label className="control-label danger pt-2">¿El evaluado ha participado en eventos planeados o no planeados atendidos bajo el SCI fuera de su país?</label>
+                    <SelectSiNo
+                        className="form-control myInput"
+                        name='eventos_plnaeados_sci_fuera'
+                        onChange={setInfo}
+                    />
+                </div>
 
-            {/* ¿El evaluado ha ocupado en eventos planeados o no estructura...? */}
-            <div className='col-12'>
-                <label className="control-label danger pt-2">¿El evaluado ha ocupado en eventos planeados o no planeados alguna posición dentro de la estructura del SCI?</label>
-                <SelectSiNo
-                    className="form-control myInput"
-                    name='eventos_plnaeados_dentro_estructura'
-                    onChange={setInfo}
-                />
-            </div>
+                {/* ¿El evaluado ha ocupado en eventos planeados o no estructura...? */}
+                <div className='col-12'>
+                    <label className="control-label danger pt-2">¿El evaluado ha ocupado en eventos planeados o no planeados alguna posición dentro de la estructura del SCI?</label>
+                    <SelectSiNo
+                        className="form-control myInput"
+                        name='eventos_plnaeados_dentro_estructura'
+                        onChange={setInfo}
+                    />
+                </div>
 
-            {/* SCI/SMI 200 */}
-            <div className='col-5'>
-                <label className="control-label pt-2">Si la respuesta fue “SI” indique cual</label>
-                <input
-                    className="form-control myInput"
-                    name='sci_cual'
-                    type=''
-                    onChange={setInfo}
-                    placeholder='Indique cual...'
-                />
-            </div>
+                {/* SCI/SMI 200 */}
+                <div className='col-5'>
+                    <label className="control-label pt-2">Si la respuesta fue “SI” indique cual</label>
+                    <input
+                        className="form-control myInput"
+                        name='sci_cual'
+                        type=''
+                        onChange={setInfo}
+                        placeholder='Indique cual...'
+                    />
+                </div>
 
-            {/* ¿El evaluado pertenece a algún Equipo de Manejo de Incidentes? */}
-            <div className='col-7'>
-                <label className="control-label danger pt-2">¿El evaluado pertenece a algún Equipo de Manejo de Incidentes?</label>
-                <SelectSiNo
-                    className="form-control myInput"
-                    name='evaluado_menejo_incidentes'
-                    onChange={setInfo}
-                />
-            </div>
+                {/* ¿El evaluado pertenece a algún Equipo de Manejo de Incidentes? */}
+                <div className='col-7'>
+                    <label className="control-label danger pt-2">¿El evaluado pertenece a algún Equipo de Manejo de Incidentes?</label>
+                    <SelectSiNo
+                        className="form-control myInput"
+                        name='evaluado_menejo_incidentes'
+                        onChange={setInfo}
+                    />
+                </div>
 
-            <h4><br/>Examen SCI-100 / SCI-200</h4>
-            
+            </React.Fragment>}
+
+            {/* <h4><br />Examen SCI-100 / SCI-200</h4>
+
             <SCI100></SCI100>
 
             <h4><br />Examen S-190 / S-130</h4>
 
-            <S190></S190>
+            <S190></S190> */}
 
-            {/* BTN Continuar */}
+            {/* BTN SCI/SMI 100 */}
+            <div className='col-12 pt-5 btn-margin'>
+                {/* TODO: completar examenes */}
+                <button
+                    hidden={(state.eventos_plnaeados_sci &&
+                        state.eventos_plnaeados_sci_fuera &&
+                        state.eventos_plnaeados_dentro_estructura &&
+                        state.evaluado_menejo_incidentes && !smi_100Examen) ? false : true}
+                    
+                    onClick={siguienteExamen}
+                    className='btn btn-warning'
+                // onClick={checkData}
+                >Tomar examen SCI/SMI 100</button>
+            </div>
+
+
             <div className='col-12 pt-5 btn-margin'>
                 <button
+                    disabled={!smi_100Examen}
                     className='btn btn-primary'
                     onClick={checkData}
                 >Continuar</button>
--- src/components/S5.jsx
+++ src/components/S5.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AlertExito from '../singles/AlertExito'
 
 const S5 = (props) => {
     const { state, setState, checkData } = props
+    const [preguntas_s_190, setPreguntas_s_190] = useState(false)
+    const [sci_190Examen, setSci_190Examen] = useState(false)
 
     const setInfo = (input) => {
         /* setea al state las variables */
@@ -10,17 +13,26 @@ const S5 = (props) => {
             [input.target.name]: input.target.value
         })
     }
+
+    const siguienteExamen = () => {
+
+        AlertExito('Examen')
+        setSci_190Examen(true)
+    }
+
     return (
         <div className='row body_wrap'>
-            <h1>EXAMENES</h1>
+
             {/* S-190 */}
             <div className='col-6'>
                 <label className="control-label pt-2">S-190 o CPCIF</label>
                 <input
                     className="form-control myInput"
-                    name='s-190'
+                    name='s_190'
                     type='file'
                     onChange={setInfo}
+                    onBlur={() => { setPreguntas_s_190((state.s_190 && state.s_130) ? true : false) }}
+                    onMouseLeave={() => { setPreguntas_s_190((state.s_190 && state.s_130) ? true : false) }}
                     placeholder='Ingrese S-190 o CPCIF...'
                 />
             </div>
@@ -30,42 +42,60 @@ const S5 = (props) => {
                 <label className="control-label pt-2">S-130 o CPCIF</label>
                 <input
                     className="form-control myInput"
-                    name='s-130'
+                    name='s_130'
                     type='file'
                     onChange={setInfo}
+                    onBlur={() => { setPreguntas_s_190((state.s_190 && state.s_130) ? true : false) }}
+                    onMouseLeave={() => { setPreguntas_s_190((state.s_190 && state.s_130) ? true : false) }}
                     placeholder='Ingrese S-130 o CPCIF...'
                 />
             </div>
 
-            {/* S-130 */}
-            <div className='col-12'>
-                <label className="control-label pt-2">¿Cuántas veces ha sido asignado como recurso nacional en incendios forestales en una entidad federativa distinta a su base?</label>
-                <input
-                    className="form-control myInput"
-                    name='asignado_recurso_nacional'
-                    type=''
-                    onChange={setInfo}
-                    placeholder='¿Cuántas veces ha sido asignado como recurso nacional en incendios forestales...'
-                />
+            {preguntas_s_190 && <React.Fragment>
+                {/* S-130 */}
+                <div className='col-12'>
+                    <label className="control-label pt-2">¿Cuántas veces ha sido asignado como recurso nacional en incendios forestales en una entidad federativa distinta a su base?</label>
+                    <input
+                        className="form-control myInput"
+                        name='asignado_recurso_nacional'
+                        type=''
+                        onChange={setInfo}
+                        placeholder='¿Cuántas veces ha sido asignado como recurso nacional en incendios forestales...'
+                    />
+                </div>
+
+                {/* S-130 */}
+                <div className='col-12'>
+                    <label className="control-label pt-2">¿Cuántas veces ha sido asignado como recurso en incendios forestales en otro país?</label>
+                    <input
+                        className="form-control myInput"
+                        name='asignado_recurso_otro_pais'
+                        type=''
+                        onChange={setInfo}
+                        placeholder='¿Cuántas veces ha sido asignado como recurso en incendios forestales en otro país?'
+                    />
+                </div>
+
+
+            </React.Fragment>}
+            
+            {/* BTN SCI/SMI 100 */}
+            <div className='col-12 pt-5 btn-margin'>
+                {/* TODO: completar examenes */}
+                <button
+                    hidden={(state.asignado_recurso_nacional && state.asignado_recurso_otro_pais && !sci_190Examen) ? false : true}
+
+                    onClick={siguienteExamen}
+                    className='btn btn-warning'
+                // onClick={checkData}
+                >Tomar examen SCI/SMI 100</button>
             </div>
 
-            {/* S-130 */}
-            <div className='col-12'>
-                <label className="control-label pt-2">¿Cuántas veces ha sido asignado como recurso en incendios forestales en otro país?</label>
-                <input
-                    className="form-control myInput"
-                    name='asignado_recurso_otro_pais'
-                    type=''
-                    onChange={setInfo}
-                    placeholder='¿Cuántas veces ha sido asignado como recurso en incendios forestales en otro país?'
-                />
-            </div>
-
-            <h4><br />Liga a examen</h4>
 
             {/* BTN Continuar */}
             <div className='col-12 pt-5 btn-margin'>
                 <button
+                    disabled={!sci_190Examen}
                     className='btn btn-primary'
                     onClick={checkData}
                 >Continuar</button>
